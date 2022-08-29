6835f639379b9ed3807756f36f6b836d24b27b45
James Adakole <j.adakole@taxaide.com.ng>
2022-08-29 23:35:47 +0100

    finish navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -38,8 +38,10 @@ const Navbar = () => {
         )}
 
         {isOpen && (
-          <ul 
-          className="z-10 fixed top-0 -right-2 p-3 ">
+          <ul className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden 
+          list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in
+          "
+          >
             <li className="text-xl w-full my-2">
               <AiOutlineClose
                 onClick={() => setIsOpen(false)}
@@ -49,7 +51,11 @@ const Navbar = () => {
             </li>
             {['Market', 'Exchange', 'Tutorials', 'Wallet'].map(
               (item, index) => (
-                <NavbarItem key={item + index} title={item} classProps="my-2 text-lg" />
+                <NavbarItem
+                  key={item + index}
+                  title={item}
+                  classProps="my-2 text-lg"
+                />
               ),
             )}
           </ul>
